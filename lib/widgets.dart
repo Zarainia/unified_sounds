d631fec5e4fc38d9b22c4be1f0f65d3389a7baf4
zarainia <jessie98@gmail.com>
2021-08-17 17:44:06 -0400

    Added keyboard controls to widgets

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,16 +1,42 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 
 import 'sounds.dart';
+
+Duration _duration_clamp(Duration duration, Duration min, Duration max) {
+  if (duration < min) return min;
+  if (duration > max) return max;
+  return duration;
+}
+
+KeyEventResult _control_sound_via_keyboard(RawKeyEvent event, AudioPlayer sound_player) {
+  print("pressed");
+  if (event.isKeyPressed(LogicalKeyboardKey.space)) {
+    sound_player.play_or_pause();
+    return KeyEventResult.handled;
+  }
+  var position = sound_player.status.position;
+  var duration = sound_player.status.duration;
+  if (position != null && duration != null) {
+    if (event.isKeyPressed(LogicalKeyboardKey.arrowLeft))
+      sound_player.seek(_duration_clamp(position - Duration(seconds: 10), Duration.zero, duration));
+    else if (event.isKeyPressed(LogicalKeyboardKey.arrowRight)) sound_player.seek(_duration_clamp(position + Duration(seconds: 10), Duration.zero, duration));
+    return KeyEventResult.handled;
+  }
+  return KeyEventResult.ignored;
+}
 
 class PlaybackControls extends StatelessWidget {
   AnimationController animation_controller;
   AudioPlayer sound_player;
   double play_icon_size;
+  VoidCallback? on_click;
 
-  PlaybackControls({required this.animation_controller, required this.sound_player, this.play_icon_size = 35});
+  PlaybackControls({required this.animation_controller, required this.sound_player, this.play_icon_size = 35, this.on_click});
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +54,7 @@ class PlaybackControls extends StatelessWidget {
           style: ElevatedButton.styleFrom(primary: themedata.primaryColor, shape: CircleBorder(), padding: EdgeInsets.all(15)),
           onPressed: () {
             sound_player.play_or_pause();
+            on_click?.call();
           },
         ),
       ],
@@ -123,48 +150,51 @@ class _DisplaySoundPlayerState extends State<DisplaySoundPlayer> with TickerProv
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        SizedBox.expand(
-          child: widget.display_widget,
-        ),
-        Positioned(
-          child: Stack(
-            children: [
-              Column(
+    return FocusScope(
+        child: Stack(
+          children: [
+            SizedBox.expand(
+              child: widget.display_widget,
+            ),
+            Positioned(
+              child: Stack(
                 children: [
-                  SizedBox(height: 25),
-                  Container(
-                    child: PlaybackSlider(
-                      sound_player: sound_player,
-                      playback_limit: playback_limit,
-                      playback_position: playback_position,
-                      on_change: (double new_position) {
-                        setState(() {
-                          playback_position = Duration(milliseconds: new_position.toInt());
-                        });
-                      },
-                      position_colour: widget.position_colour,
-                    ),
-                    padding: EdgeInsets.only(top: 20, left: 10, right: 10),
-                    color: widget.shadow_colour,
+                  Column(
+                    children: [
+                      SizedBox(height: 25),
+                      Container(
+                        child: PlaybackSlider(
+                          sound_player: sound_player,
+                          playback_limit: playback_limit,
+                          playback_position: playback_position,
+                          on_change: (double new_position) {
+                            setState(() {
+                              playback_position = Duration(milliseconds: new_position.toInt());
+                            });
+                          },
+                          position_colour: widget.position_colour,
+                        ),
+                        padding: EdgeInsets.only(top: 20, left: 10, right: 10),
+                        color: widget.shadow_colour,
+                      ),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  ),
+                  PlaybackControls(
+                    animation_controller: button_animation_controller,
+                    sound_player: sound_player,
                   ),
                 ],
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                clipBehavior: Clip.none,
               ),
-              PlaybackControls(
-                animation_controller: button_animation_controller,
-                sound_player: sound_player,
-              ),
-            ],
-            clipBehavior: Clip.none,
-          ),
-          bottom: 0,
-          left: 0,
-          right: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+            ),
+          ],
         ),
-      ],
-    );
+        autofocus: true,
+        onKey: (_, event) => _control_sound_via_keyboard(event, sound_player));
   }
 }
 
@@ -172,8 +202,9 @@ class SimpleSoundPlayer extends StatefulWidget {
   String name;
   Uint8List contents;
   Color? position_colour;
+  Color? focused_colour;
 
-  SimpleSoundPlayer({required this.name, required this.contents, this.position_colour});
+  SimpleSoundPlayer({required this.name, required this.contents, this.position_colour, this.focused_colour});
 
   @override
   _SimpleSoundPlayerState createState() => _SimpleSoundPlayerState();
@@ -184,6 +215,8 @@ class _SimpleSoundPlayerState extends State<SimpleSoundPlayer> with TickerProvid
   Duration playback_position = Duration();
   Duration playback_limit = Duration();
   late AnimationController button_animation_controller;
+  bool focused = false;
+  late FocusScopeNode focus_node;
 
   @override
   void initState() {
@@ -199,6 +232,7 @@ class _SimpleSoundPlayerState extends State<SimpleSoundPlayer> with TickerProvid
       button_animation_controller.animateTo(status.is_playing ? 0 : 1);
     };
     button_animation_controller = AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    focus_node = FocusScopeNode();
   }
 
   @override
@@ -210,31 +244,54 @@ class _SimpleSoundPlayerState extends State<SimpleSoundPlayer> with TickerProvid
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        child: Column(
-          children: [
-            PlaybackControls(
-              animation_controller: button_animation_controller,
-              sound_player: sound_player,
-              play_icon_size: 30,
+    return FocusScope(
+      child: Card(
+        child: InkWell(
+          child: Container(
+            child: Column(
+              children: [
+                PlaybackControls(
+                  animation_controller: button_animation_controller,
+                  sound_player: sound_player,
+                  play_icon_size: 30,
+                  on_click: () {
+                    Future.delayed(const Duration(milliseconds: 50), () {
+                      focus_node.requestFocus();
+                    });
+                  },
+                ),
+                PlaybackSlider(
+                  sound_player: sound_player,
+                  playback_limit: playback_limit,
+                  playback_position: playback_position,
+                  on_change: (double new_position) {
+                    setState(() {
+                      playback_position = Duration(milliseconds: new_position.toInt());
+                    });
+                  },
+                  position_colour: widget.position_colour,
+                ),
+              ],
             ),
-            PlaybackSlider(
-              sound_player: sound_player,
-              playback_limit: playback_limit,
-              playback_position: playback_position,
-              on_change: (double new_position) {
-                setState(() {
-                  playback_position = Duration(milliseconds: new_position.toInt());
-                });
-              },
-              position_colour: widget.position_colour,
-            ),
-          ],
+            constraints: BoxConstraints(maxWidth: 300),
+            padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+          ),
+          onTap: () {
+            Future.delayed(const Duration(milliseconds: 50), () {
+              focus_node.requestFocus();
+            });
+          },
+          excludeFromSemantics: true,
         ),
-        constraints: BoxConstraints(maxWidth: 300),
-        padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+        color: focused ? widget.focused_colour : null,
       ),
+      node: focus_node,
+      onKey: (_, event) => _control_sound_via_keyboard(event, sound_player),
+      onFocusChange: (now_focused) {
+        setState(() {
+          focused = now_focused;
+        });
+      },
     );
   }
 }
